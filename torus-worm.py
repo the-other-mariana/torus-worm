2dc8e1a3a0440da09812d4629138d09637daeab0
Mariana Avalos <mariana.avalos.arce@gmail.com>
2020-07-11 16:56:53 -0500

    points as spheres

---
+++ torus-worm.py
@@ -0,0 +1,24 @@
+#File:      torus-worm.py
+#Author:    Mariana Avalos
+#Date:      11/07/2020
+#Description: Python code that makes an animated torus
+
+import maya.cmds as c
+import math as math
+
+sections = 6
+radius = 5
+PI = 3.1416
+z = 0
+step_angle = 360.0 / sections
+
+for i in range(sections):
+    rads = (step_angle * PI / 180.0)
+    x = radius * math.cos(i * rads - (rads / 2.0))
+    y = radius * math.sin(i * rads - (rads / 2.0))
+
+    # points as spheres
+    c.polySphere(sx = 5, sy = 5, r = 0.5, n = 'point{}'.format(i))
+    c.setAttr('point{}.translateX'.format(i), x)
+    c.setAttr('point{}.translateZ'.format(i), z)
+    c.setAttr('point{}.translateY'.format(i), y)
