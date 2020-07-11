059cc627a83cb557b0716028d71d7403a5673338
Mariana Avalos <mariana.avalos.arce@gmail.com>
2020-07-11 17:55:45 -0500

    torus vertices as spheres

--- torus-worm.py
+++ torus-worm.py
@@ -6,20 +6,34 @@
 import maya.cmds as c
 import math as math
 
+tubes = 9
 sections = 6
 inner_radius = 3
 distance = 5
 PI = 3.1416
 z = 0
-step_angle = 360.0 / sections
+step_outer_angle = 360.0 / tubes
+step_inner_angle = 360.0 / sections
 
-for i in range(sections):
-    rads = (step_angle * PI / 180.0)
-    x = distance + inner_radius * math.cos(i * rads - (rads / 2.0))
-    y = inner_radius * math.sin(i * rads - (rads / 2.0))
+for j in range(tubes):
+    outer_rads = (step_outer_angle * PI / 180.0)
+    outer_distance = 0.0
+    x = 0.0
+    y = 0.0
+    z = 0.0
+    for i in range(sections):
+        inner_rads = (step_inner_angle * PI / 180.0)
+        inner_x = distance + inner_radius * math.cos(i * inner_rads - (inner_rads / 2.0))
+        inner_y = inner_radius * math.sin(i * inner_rads - (inner_rads / 2.0))
 
-    # points as spheres
-    c.polySphere(sx = 5, sy = 5, r = 0.5, n = 'point{}'.format(i))
-    c.setAttr('point{}.translateX'.format(i), x)
-    c.setAttr('point{}.translateZ'.format(i), z)
-    c.setAttr('point{}.translateY'.format(i), y)
+        outer_distance = inner_x
+        y = inner_y
+
+        x = outer_distance * math.sin(j * outer_rads - (outer_rads / 2.0))
+        z = outer_distance * math.cos(j * outer_rads - (outer_rads / 2.0))
+
+        # points as spheres
+        c.polySphere(sx = 5, sy = 5, r = 0.5, n = 'point{}{}'.format(i, j))
+        c.setAttr('point{}{}.translateX'.format(i, j), x)
+        c.setAttr('point{}{}.translateZ'.format(i, j), z)
+        c.setAttr('point{}{}.translateY'.format(i, j), y)
