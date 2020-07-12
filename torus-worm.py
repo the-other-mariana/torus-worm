89a8ec373ad1375e374c2581faba3742d93b6a60
Mariana Avalos <mariana.avalos.arce@gmail.com>
2020-07-11 20:13:24 -0500

    classic torus for n sections and tubes

--- torus-worm.py
+++ torus-worm.py
@@ -5,9 +5,15 @@
 
 import maya.cmds as c
 import math as math
+import maya.OpenMaya as OM
 
-tubes = 9
-sections = 6
+verts = []
+mesh = OM.MFnMesh()
+mergeVerts = True
+pointTolerance = 0.001
+
+tubes = 5
+sections = 5
 inner_radius = 3
 distance = 5
 PI = 3.1416
@@ -28,7 +34,6 @@ for j in range(tubes):
 
         outer_distance = inner_x
         y = inner_y
-
         x = outer_distance * math.sin(j * outer_rads - (outer_rads / 2.0))
         z = outer_distance * math.cos(j * outer_rads - (outer_rads / 2.0))
 
@@ -37,3 +42,58 @@ for j in range(tubes):
         c.setAttr('point{}{}.translateX'.format(i, j), x)
         c.setAttr('point{}{}.translateZ'.format(i, j), z)
         c.setAttr('point{}{}.translateY'.format(i, j), y)
+
+        # save vertices
+        temp = OM.MPoint(x, y, z)
+        verts.append(temp)
+
+quadArray = OM.MPointArray()
+quadArray.setLength(4)
+print(len(verts))
+
+
+for j in range(tubes):
+    exception = 0
+    print("-----")
+    for i in range(sections):
+        print("+++++")
+        if i == sections - 1:
+            exception = sections
+        quadArray.set(verts[(i) + (sections)* j], 0)
+        print(str((i) + (sections)* j))
+        quadArray.set(verts[(i + 1) - exception + (sections)* j], 1)
+        print(str((i + 1) - exception + (sections)* j))
+
+        if j == tubes - 1:
+            var = 8 + sections
+            quadArray.set(verts[((i + sections + 1) - exception + (sections)* j) % len(verts)], 2)
+            print(str(((i + sections + 1) - exception + (sections)* j) % len(verts)))
+            quadArray.set(verts[((i + sections) + (sections)* j) % len(verts)], 3)
+            print(str(((i + sections) + (sections)* j) % len(verts)))
+        else:
+
+            quadArray.set(verts[(i + sections + 1) - exception + (sections)* j], 2)
+            print(str((i + sections + 1) - exception + (sections)* j))
+            quadArray.set(verts[(i + sections) + (sections)* j], 3)
+            print(str((i + sections) + (sections)* j))
+
+
+
+        mesh.addPolygon(quadArray, mergeVerts, pointTolerance)
+
+'''
+for j in range(tubes-1):
+    exception = 0
+    for i in range(sections):
+        if(i == sections -1):
+            exception = sections
+        quadArray.set(verts[(i + 1) + (sections * j)], 0)
+        print(str((i + 1) + (sections * j)))
+        quadArray.set(verts[(i + 2 - exception) + (sections * j)], 1)
+        print(str((i + 2 - exception) + (sections * j)))
+        quadArray.set(verts[(i + (sections + 2) - exception) + (sections * j)], 2)
+        print(str((i + (sections + 2) - exception) + (sections * j)))
+        quadArray.set(verts[(i + (sections + 1)) + (sections * j)], 3)
+        print(str((i + (sections + 1)) + (sections * j)))
+        mesh.addPolygon(quadArray, mergeVerts, pointTolerance)
+'''
